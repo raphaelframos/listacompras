69a066e909886b1eccfb9f88923fbb7d193fb161
Raphael <raphaelframos@gmail.com>
2020-01-13 03:02:21 -0300

    Auto completando com os nomes dos produtos

--- app/src/main/java/com/powellapps/compraparamim/ui/newlist/NewListActivity.kt
+++ app/src/main/java/com/powellapps/compraparamim/ui/newlist/NewListActivity.kt
@@ -2,26 +2,27 @@ package com.powellapps.compraparamim.ui.newlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.ImageButton
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.textfield.TextInputEditText
 import com.powellapps.compraparamim.R
 import com.powellapps.compraparamim.repository.FirebaseRepository
-import com.powellapps.compraparamim.utils.Utils
 
 class NewListActivity : AppCompatActivity() {
 
     var adapter = ProductAdapter()
+    lateinit var editTextName : AutoCompleteTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_list)
 
+        editTextName = findViewById(R.id.textInputEditText_name)
         val recyclerViewProducts = findViewById<RecyclerView>(R.id.recyclerView_products)
         recyclerViewProducts.layoutManager = LinearLayoutManager(this)
         recyclerViewProducts.adapter = adapter
@@ -30,15 +31,37 @@ class NewListActivity : AppCompatActivity() {
         val viewModel = ViewModelProviders.of(this).get(ViewModelNewList::class.java)
         viewModel.getProducts("1").observe(this, Observer {
             adapter.update(it)
+            setNames(it)
         })
+
+
+
+
+
 
         val imageButtonSend = findViewById<ImageButton>(R.id.imageButton_send)
         imageButtonSend.setOnClickListener({
-            val editTextName : TextInputEditText = findViewById(R.id.textInputEditText_name)
+
             val name = editTextName.text.toString()
             val product = Product(name)
             FirebaseRepository().save(product)
+            editTextName.setText("")
 
         })
+    }
+
+    fun setNames(it: List<Product>) {
+        var names = ArrayList<String>()
+        it.forEach {
+            if(!names.contains(it.name)){
+                names.add(it.name)
+            }
+        }
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            names
+        )
+        editTextName.setAdapter(adapter)
     }
 }
